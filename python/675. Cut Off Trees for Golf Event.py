9061b6864e45e1c770160eb93f9c552d66dd9ca5
v_zhuilin <v_zhuilin@tencent.com>
2022-05-24 14:57:50 +0800

    update

--- python/675. Cut Off Trees for Golf Event.py
+++ python/675. Cut Off Trees for Golf Event.py
@@ -7,33 +7,43 @@ class Solution:
 
         def bfs(sx,sy,i,j):
             m,n = len(forest), len(forest[0])
-            visted = []
+            visted = {(sx,sy)}
             q = deque([(0,sx,sy)])
             while q:
                 d,i_,j_ = q.popleft()
                 if i_ == i and j_ == j:
                     return d
                 for (x,y) in [(i_+1, j_),(i_-1, j_),(i_, j_+1),(i_, j_-1)]:
-                    if 0 <= x <= m  and 0 <= y <= n and (x,y) not in visted and  forest[x][y]:
-                        visted.append((x,y))
-                        d+=1
-                        q.append((d,x,y))
+                    if 0 <= x < m  and 0 <= y < n and (x,y) not in visted and  forest[x][y]:
+                        visted.add((x,y))
+                        if x == i and y == j:
+                            return d + 1
+
+                        q.append((d+1,x,y))
+            return -1
 
 
 
         # sorted tress
-        trees = sorted((h,i,j)for i,row in enumerate(forest) for h,j in enumerate(row))
+        trees = sorted((h,i,j)for i,row in enumerate(forest) for j,h in enumerate(row) if h >0)
         sx= sy= d =0
         for _ ,i,j in trees:
-            d = bfs(sx,sy,i,j)
-            if  d<0:
+            d_ = bfs(sx,sy,i,j)
+            if  d_<0:
                 return -1 #
-            d +=d
+            d +=d_
             sx,sy = i,j
         return d
 if __name__ == '__main__':
-    forest =[[1,2,3],[0,0,4],[7,6,5]]
+    forest =[[4,2,3],[0,0,1],[7,6,5]]
     ret = Solution().cutOffTree(forest)
     print(ret)
+    """
+    输入：
+[[4,2,3],[0,0,1],[7,6,5]]
+输出：
+14
+预期结果：
+10"""
 
 
