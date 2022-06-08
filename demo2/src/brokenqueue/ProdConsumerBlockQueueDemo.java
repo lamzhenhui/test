f7ae884a48f9717baf3b43cb6c222649cc4f77ea
lamzhenhui <lamzhenhui>
2022-06-08 23:56:29 +0800

    update

--- demo2/src/brokenqueue/ProdConsumerBlockQueueDemo.java
+++ demo2/src/brokenqueue/ProdConsumerBlockQueueDemo.java
@@ -6,85 +6,83 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class ShardData{
-    private  AtomicInteger number = new AtomicInteger(1);
+    private  AtomicInteger number = new AtomicInteger();
     private volatile  boolean flag = true;
-    private BlockingQueue blockingQueue = null;
+    private BlockingQueue<String> blockingQueue = null;
+    public  ShardData(BlockingQueue<String> blockingQueue){
+        this.blockingQueue = blockingQueue;
+        System.out.println(this.blockingQueue.getClass().getName());
+
+    }
     public void  my_prod(BlockingQueue blockingQueue){
         boolean return_value;
+        String data;
         while(flag){
-            String data = number.getAndIncrement()+"";
+            data = number.getAndIncrement()+""; // 这里应该用引用，
             try {
                 return_value = blockingQueue.offer(data,2,TimeUnit.SECONDS);
                 if (return_value){
-
-                    System.out.println("插入队列成功");
+                    System.out.println("插入队列" + data + "成功");
                 }else {
-                    System.out.println("插入队列失败");
-
+                    System.out.println("插入队列" + data + "失败");
                 }
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-
-
         }
         System.out.println("老板喊停, 停止生产");
     }
 
-    public void  my_consumer(){
-        String result =null;
+    public void  my_consumer() throws Exception{
+        String return_value = null;
         while(flag){
-            if (number !=null || "".equalsIgnoreCase(number+"")){}
-                try {
-                    result = blockingQueue.poll(2,TimeUnit.SECONDS);
-                    if (return_value){
-
-                        System.out.println("插入队列成功");
-                    }else {
-                        System.out.println("插入队列失败");
-
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
+            try {
+                return_value = (String) blockingQueue.poll(2,TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (return_value == null || "".equalsIgnoreCase(return_value)){
+                flag=false;
+                System.out.println("查询2秒仍然无法获取数据, 消费停止");
+                return ;
+            }else {
+                System.out.println("查询队列" + return_value + "成功");
+            }
 
         }
-        System.out.println("老板喊停, 停止生产");
     }
-
-
 
     public void set_stop(){
-        this.flag = false;
+        flag = false;
     }
 
 }
 public class ProdConsumerBlockQueueDemo {
     public static void main(String[] args) {
-        ShardData shardData = new ShardData();
-        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
+        ShardData shardData = new ShardData(blockingQueue);
         new Thread(() ->{
             shardData.my_prod(blockingQueue);
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         },"aa ").start();
 
 
         new Thread(() ->{
-            shardData.my_consumer(blockingQueue);
+            try {
+                shardData.my_consumer();
+//                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         },"bb ").start();
 
         try {
-            TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("5秒后老板喊停");
         shardData.set_stop();
-        System.out.println("老板喊停");
 
 
     }
