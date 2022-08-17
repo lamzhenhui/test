3ae19aa4c69422060a2c0fd98a1ea8a9f77e86c8
lamzhenhui <lamzhenhui>
2022-08-17 17:33:27 +0800

    dasuibain

---
+++ go_demo/chain_demo/chain_demo.go
@@ -0,0 +1,14 @@
+package chain_demo
+
+import "fmt"
+
+func run() {
+
+	ch1 := make(chan int, 3)
+	ch1 <- 2
+	ch1 <- 1
+	ch1 <- 3
+	elem1 := <-ch1
+	fmt.Printf("The first element received from channel ch1: %v\n",
+		elem1)
+}
--- go_demo/main.go
+++ go_demo/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	print(123)
-	d :=Dog{
+	d := Dog{
 		name: "dog1",
 	}
-	fmt.Printf("dog %s",d)
+
+	fmt.Printf("dog %s", d)
+
+	ch1 := make(chan int, 3)
+	ch1 <- 2
+	ch1 <- 1
+	ch1 <- 3
+	elem1 := <-ch1
+	fmt.Printf("The first element received from channel ch1: %v\n",
+		elem1)
 }
+
 type Dog struct {
 	name string
 }
